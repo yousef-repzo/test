6f4de9d82aeee25aa2069ab240fc01f85499b0b6
Yousef Alshun <yousef.alshun@repzoapp.com>
2023-11-29 15:15:26 +0300

    fsd

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ function App() {
   return (
     <>
       <div>
+        {getVal()}
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
