5367d3dff9b77cbe9f72a180b833b2d315103768
Yousef Alshun <yousef.alshun@repzoapp.com>
2023-11-29 15:17:15 +0300

    sfd

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-const realKeys: any = {};
-const filterKey = "";
+const realKeys: any = { hi: "hi" };
+const filterKey = "hi";
 
 function App() {
   const [count, setCount] = useState(0);
